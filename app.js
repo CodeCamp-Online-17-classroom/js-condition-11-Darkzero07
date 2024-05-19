79e00c716f93b3826e4edcec77a15beaed219425
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 10:25:15 +0700

    js lab 28

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+let user = prompt("Who is login?  ");
+if (user !== "codecamp") {
+  user = "guest";
+} else {
+  let password = prompt("Enter your password :");
+  if (password == "123456") {
+  } else {
+    alert("Wrong password");
+    prompt("Enter your password :");
+  }
+}
+
+alert(`Welcome ${user}`);
